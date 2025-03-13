114ca71a4d4f21aabb7f8149dc732dee18062fef
fu24 <nontasak.arun@bumail.net>
2025-03-13 10:50:06 +0700

    last

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -1,14 +1,31 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
-
     Rigidbody rb;
     const float G = 0.0674f;
+    public static List<Gravity> gravityObjectList;
+   
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        if (gravityObjectList == null)
+        {
+            gravityObjectList = new List<Gravity>();
+        }
+        gravityObjectList.Add(this);
+    }
+    private void FixedUpdate()
+    {
+        foreach (var obj in gravityObjectList)
+        {
+            if (obj != this) 
+            {
+                Attract(obj);
+            }
+        }
     }
     void Attract(Gravity other)
     {
